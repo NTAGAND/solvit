95e0f219a4923b2c16971a4f4ee453cbbad90557
NTAGAND <92169827+NTAGAND@users.noreply.github.com>
2023-02-08 02:32:44 -0800

    index commit

--- index.py
+++ index.py
@@ -1,15 +1,34 @@
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    sum = 0
-    average = float()
-   
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
+import sys 
+
+n = int(input("Enter number of student btn 2 & 10 : "))
+
+student_marks = {}
+
+if n < 2 or n >10:
+    print("Invalid number,Please Enter number btn 2 and 10 ")
+else:
+    for r in range(n):
+        line = input().split()
+        name, scores = line[0], line[1:]
+        scores = list(map(float, scores))
+
+        if len(scores) > 3:
+            print("Please Enter Marks not greater than 3")
+            sys.exit()
+        elif len(scores) < 3:
+            print("Please Enter marks not less than 3")
+            sys.exit()
+        else:
+            for q in scores:
+                if float(q) > 100 or float(q) < 0:
+                    print("invalid marks, Marks must be between 0 and 100.")
+                    sys.exit()
+                else:
+                    student_marks[name] = scores
+                
     query_name = input()
+    marks=0
     for i in student_marks[query_name]:
-        sum += i
-    average = sum / len(student_marks[query_name])
-    print("%.2f" %average)
+        marks=marks+i
+    Average=marks/3
+    print("%.2f"%Average)
